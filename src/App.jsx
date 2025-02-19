2fb54de04e63145f908f600cb485ca31569b3510
MShubar <pokeeeeepow@gmail.com>
2025-02-20 00:25:45 +0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
+import Home from './pages/home/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Signin from './pages/auth/Signin'
